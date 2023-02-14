ee12723e08bd728993c1faaefddaba0ba3942c17
johnhitz203 <johnhitz203@gmail.com>
2023-02-14 16:56:07 -0600

    working in class

--- lib/math.ex
+++ lib/math.ex
@@ -1,18 +1,27 @@
 defmodule Math do
-  @moduledoc """
-  Documentation for `Math`.
-  """
-
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Math.hello()
-      :world
-
+  Add two integers
   """
-  def hello do
-    :world
+  def add(int1, int2) when is_integer(int1) and is_integer(int2) do
+    int1 + int2
+  end
+
+  def add(str1, str2) when is_binary(str1) and is_binary(str2) do
+    str1 <> str2
+  end
+
+  def add(list1, list2) do
+    # list1 ++ list2 => r, g, r
+    # => r, g
+    # [list1 | list2]
+    [list1 | list2] |> List.flatten()
+  end
+
+  def subtract(int1, int2) when is_integer(int1) and is_integer(int2) do
+    int1 - int2
+  end
+
+  def subtract(list1, list2) do
+    list1 -- list2
   end
 end
--- test/math_test.exs
+++ test/math_test.exs
@@ -2,7 +2,42 @@ defmodule MathTest do
   use ExUnit.Case
   doctest Math
 
-  test "greets the world" do
-    assert Math.hello() == :world
+  # describe "Integer Math" do
+  #   test "add/2 w/ integers" do
+  #     assert Math.add(1, 1) == 2
+  #     assert Math.add(0, 1) == 1
+  #     assert Math.add(-1, 1) == 0
+  #   end
+  # end
+
+  describe "add/2" do
+    test "add/2 w/ integers" do
+      assert Math.add(1, 1) == 2
+      assert Math.add(0, 1) == 1
+      assert Math.add(-1, 1) == 0
+    end
+
+    test "add/2 with strings" do
+      assert Math.add("foo", "bar") == "foobar"
+    end
+
+    test "add/2 w/ lists of single values" do
+      assert Math.add([1], [2]) == [1, 2]
+      # assert Math.add([1, 2], [3, 4]) == [1, 2, 3, 4]
+    end
+
+    test "add/2 w/ lists of multiple values" do
+      assert Math.add([1, 2], [3, 4]) == [1, 2, 3, 4]
+    end
+  end
+
+  describe "subtract/2" do
+    test "subtract/2 integers" do
+      assert Math.subtract(1, 1) == 0
+    end
+
+    test "subtract/2 list w/ single values" do
+      assert Math.subtract([1], [2]) == [1]
+    end
   end
 end
