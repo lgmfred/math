d977a7aede011a81fcd4c358a7602a8795005e16
Ayiko Fred <lgmfred@ayikoyo.com>
2023-02-15 16:44:23 +0300

    finish math module testing exercise

--- lib/math.ex
+++ lib/math.ex
@@ -1,18 +1,26 @@
 defmodule Math do
   @moduledoc """
-  Documentation for `Math`.
+  `Math` module just for abstraction.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Math.hello()
-      :world
-
+  Add the two arguments together
   """
-  def hello do
-    :world
+  def add!(a, b) when is_integer(a) and is_integer(b), do: a + b
+  def add!(a, b) when is_binary(a) and is_binary(b), do: a <> b
+  def add!(a, b) when is_list(a) and is_list(b), do: a ++ b
+
+  @doc """
+  Subtract two arguments
+  """
+  def subtract!(a, b) when is_integer(a) and is_integer(b), do: a - b
+  def subtract!(a, b) when is_list(a) and is_list(b), do: a -- b
+
+  def subtract!(a, b) when is_binary(a) and is_binary(b) do
+    list_a = String.split(a, "", trim: true)
+    list_b = String.split(b, "", trim: true)
+
+    (list_a -- list_b)
+    |> List.to_string()
   end
 end
