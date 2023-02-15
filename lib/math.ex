19c2f88611284a1ab6f5bc77d06f67852eb9757b
Ayiko Fred <lgmfred@ayikoyo.com>
2023-02-15 16:16:29 +0300

    finish math module testing exercise

--- lib/math.ex
+++ lib/math.ex
@@ -2,17 +2,32 @@ defmodule Math do
   @moduledoc """
   Documentation for `Math`.
   """
+  def add(a, b) do
+    cond do
+      is_integer(a) and is_integer(b) -> a + b
+      is_binary(a) and is_binary(b) -> a <> b
+      is_list(a) and is_list(b) -> a ++ b
+      true -> raise ArgumentError, "#{inspect(a)} and #{inspect(b)} are invalid inputs"
+    end
+  end
 
-  @doc """
-  Hello world.
+  def subtract(a, b) do
+    cond do
+      is_integer(a) and is_integer(b) ->
+        a - b
 
-  ## Examples
+      is_binary(a) and is_binary(b) ->
+        list_a = String.split(a, "", trim: true)
+        list_b = String.split(b, "", trim: true)
 
-      iex> Math.hello()
-      :world
+        (list_a -- list_b)
+        |> List.to_string()
 
-  """
-  def hello do
-    :world
+      is_list(a) and is_list(b) ->
+        a -- b
+
+      true ->
+        raise ArgumentError, "#{inspect(a)} and #{inspect(b)} are invalid inputs"
+    end
   end
 end
--- test/math_test.exs
+++ test/math_test.exs
@@ -2,7 +2,108 @@ defmodule MathTest do
   use ExUnit.Case
   doctest Math
 
-  test "greets the world" do
-    assert Math.hello() == :world
+  describe "add/2" do
+    test "with integers" do
+      assert Math.add(2, 2) == 4
+      assert Math.add(7, 8) == 15
+      assert Math.add(7, -4) == 3
+    end
+
+    test "with strings" do
+      assert Math.add("a", "b") == "ab"
+      assert Math.add("c", "d") == "cd"
+    end
+
+    test "with lists" do
+      assert Math.add(["a", "b"], ["c", "d"]) == ["a", "b", "c", "d"]
+    end
+
+    # identity (you get the same value back)
+    test "empty string" do
+      assert Math.add("", "") == ""
+      assert Math.add("a", "") == "a"
+      assert Math.add("", "a") == "a"
+    end
+
+    test "empty list" do
+      assert Math.add([], []) == []
+      assert Math.add([1], []) == [1]
+      assert Math.add([], [1]) == [1]
+    end
+
+    test "integer + zero" do
+      assert Math.add(0, 0) == 0
+      assert Math.add(1, 0) == 1
+      assert Math.add(0, 1) == 1
+    end
+
+    test "incompatible types (integer and string)" do
+      assert_raise ArgumentError, "5 and \"5\" are invalid inputs", fn ->
+        Math.add(5, "5")
+      end
+
+      assert_raise ArgumentError, "5 and \"foo\" are invalid inputs", fn ->
+        Math.add(5, "foo")
+      end
+    end
+
+    test "invalid types (range and nil)" do
+      assert_raise ArgumentError, "1..10 and nil are invalid inputs", fn ->
+        Math.add(1..10, nil)
+      end
+    end
+  end
+
+  describe "subtract/2" do
+    test "with strings" do
+      assert Math.subtract("aa", "aa") == ""
+      assert Math.subtract("aaa", "aa") == "a"
+      assert Math.subtract("aba", "a") == "ba"
+    end
+
+    test "with integers" do
+      assert Math.subtract(2, 2) == 0
+      assert Math.subtract(3, 4) == -1
+      assert Math.subtract(17, 6) == 11
+    end
+
+    test "with lists" do
+      assert Math.subtract(["a", "a"], ["a", "a"]) == []
+      assert Math.subtract(["a", "a", "a"], ["a", "a"]) == ["a"]
+      assert Math.subtract(["a", "b", "a"], ["a"]) == ["b", "a"]
+    end
+
+    # identity (you get the same value back)
+    test "empty string" do
+      assert Math.subtract("", "") == ""
+      assert Math.subtract("a", "") == "a"
+      assert Math.subtract("", "a") == ""
+    end
+
+    test "empty list" do
+      assert Math.subtract([], []) == []
+      assert Math.subtract(["a"], []) == ["a"]
+      assert Math.subtract([], ["a"]) == []
+    end
+
+    test "integer - zero" do
+      assert Math.subtract(10, 0) == 10
+    end
+
+    test "incompatible types (integer and string)" do
+      assert_raise ArgumentError, "5 and \"5\" are invalid inputs", fn ->
+        Math.subtract(5, "5")
+      end
+
+      assert_raise ArgumentError, "5 and \"foo\" are invalid inputs", fn ->
+        Math.subtract(5, "foo")
+      end
+    end
+
+    test "invalid types (range and nil)" do
+      assert_raise ArgumentError, "1..10 and nil are invalid inputs", fn ->
+        Math.subtract(1..10, nil)
+      end
+    end
   end
 end
