70e30e80ba03801490e935fa9a97cc6239806726
Ayiko Fred <lgmfred@ayikoyo.com>
2023-02-16 18:53:22 +0300

    finish doctests reading

--- lib/math.ex
+++ lib/math.ex
@@ -2,18 +2,58 @@ defmodule Math do
   @moduledoc """
   `Math` module just for abstraction.
   """
-
+## (integers, floats, strings, lists, maps, keyword lists)
   @doc """
   Add the two arguments together
+
+  ## Examples
+
+      iex> Math.add!(3, -2)
+      1
+
+      iex> Math.add!(6.7, -0.7)
+      6.0
+
+      iex> Math.add!("foo", "baar")
+      "foobaar"
+
+      iex> Math.add!([:a, "b"], [{:c}, [:d]])
+      [:a, "b", {:c}, [:d]]
+
+      iex> Math.add!(%{a: "a"}, %{b: :b})
+      ** (FunctionClauseError) no function clause matching in Math.add!/2
+
+      iex> Math.add!([a: "a"], [b: :b])
+      [a: "a", b: :b]
   """
-  def add!(a, b) when is_integer(a) and is_integer(b), do: a + b
+  def add!(a, b) when is_number(a) and is_number(b), do: a + b
   def add!(a, b) when is_binary(a) and is_binary(b), do: a <> b
   def add!(a, b) when is_list(a) and is_list(b), do: a ++ b
 
   @doc """
   Subtract two arguments
+
+  ## Examples
+
+      iex> Math.subtract!(3, -2)
+      5
+
+      iex> Math.subtract!(6.7, -0.7)
+      7.4
+
+      iex> Math.subtract!("foobaar", "baar")
+      "foo"
+
+      iex> Math.subtract!([:a, "b", {:c}, [:d]], [{:c}, [:d]])
+      [:a, "b"]
+
+      iex> Math.subtract!(%{a: "a"}, %{b: :b})
+      ** (FunctionClauseError) no function clause matching in Math.subtract!/2
+
+      iex> Math.subtract!([a: "a", b: :b], [a: "a"])
+      [b: :b]
   """
-  def subtract!(a, b) when is_integer(a) and is_integer(b), do: a - b
+  def subtract!(a, b) when is_number(a) and is_number(b), do: a - b
   def subtract!(a, b) when is_list(a) and is_list(b), do: a -- b
 
   def subtract!(a, b) when is_binary(a) and is_binary(b) do
